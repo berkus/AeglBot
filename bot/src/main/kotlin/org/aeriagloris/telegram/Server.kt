e64a8e4e85282cb139d542b8e6e4f3a69ad99849
Berkus Karchebnyy <berkus@twilio.com>
2016-10-01 23:42:57 +0300

    Remove wasabifx - not needed yet

--- bot/src/main/kotlin/org/aeriagloris/telegram/Server.kt
+++ bot/src/main/kotlin/org/aeriagloris/telegram/Server.kt
@@ -1,13 +1,8 @@
 package org.aeriagloris.telegram
 
 import org.telegram.telegrambots.TelegramBotsApi
-import org.wasabifx.wasabi.app.AppServer
 
 fun main(args: Array<String>) {
     val telegramBotsApi = TelegramBotsApi();
     telegramBotsApi.registerBot(MeinBot());
-
-    val server = AppServer()
-    server.get("/", { response.send("Hello, go away!") })
-    server.start()
 }
