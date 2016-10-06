dcd9000bf4e9a3c3418879684b7c830429e262d7
Berkus Decker <berkus@madfire.net>
2016-10-06 17:13:04 +0300

    Remove useless response to any command

--- bot/src/main/kotlin/org/aeriagloris/telegram/AeglBot.kt
+++ bot/src/main/kotlin/org/aeriagloris/telegram/AeglBot.kt
@@ -47,21 +47,6 @@ class AeglBot : TelegramLongPollingCommandBot()
     }
 
     override fun processNonCommandUpdate(update: Update) {
-        if (update.hasMessage()) {
-            val message = update.getMessage()
-
-            //check if the message has text. it could also contain for example a location ( message.hasLocation() )
-            if (message.hasText()) {
-                //create an object that contains the information to send back the message
-                val sendMessageRequest = SendMessage()
-                sendMessageRequest.setChatId(message.getChatId().toString()) //who should get from the message the sender that sent it.
-                sendMessageRequest.setText("you said: " + message.getText())
-                try {
-                    sendMessage(sendMessageRequest)
-                } catch (e: TelegramApiException) {
-                    //do some error handling
-                }
-            }
-        }
+        // do nothing for simple chat...
     }
 }
