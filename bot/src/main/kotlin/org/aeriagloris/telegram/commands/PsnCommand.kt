798dd3ed37a88d99ec77f6b41acd7cbdf1d0dce8
Berkus Decker <berkus@madfire.net>
2016-10-13 02:17:13 +0300

    [wip] impl for /psn

--- bot/src/main/kotlin/org/aeriagloris/telegram/commands/PsnCommand.kt
+++ bot/src/main/kotlin/org/aeriagloris/telegram/commands/PsnCommand.kt
@@ -3,27 +3,31 @@ package org.aeriagloris.telegram.commands
 import org.telegram.telegrambots.TelegramApiException
 import org.telegram.telegrambots.api.methods.send.SendMessage
 import org.telegram.telegrambots.api.objects.Chat
-import org.telegram.telegrambots.api.objects.User
+import org.telegram.telegrambots.api.objects.User as TelegramUser
 import org.telegram.telegrambots.bots.AbsSender
 import org.telegram.telegrambots.logging.BotLogger
+
 import org.aeriagloris.persistence.JdbcStore
-import org.aeriagloris.persistence.UserData
+import org.aeriagloris.persistence.schema.*
+import org.jetbrains.exposed.dao.*
+import org.jetbrains.exposed.sql.transactions.transaction
 
 class PsnCommand(val store: JdbcStore) : ExtendedCommand("psn", "Link your telegram user to PSN")
 {
-    override fun execute(absSender: AbsSender, user: User, chat: Chat, arguments: Array<String>)
+    override fun execute(absSender: AbsSender, user: TelegramUser, chat: Chat, arguments: Array<String>)
     {
         if (arguments.size != 1) {
             sendReply(absSender, chat, "Usage: /psn <b>PSN id</b>", true)
             return
         }
 
-        val userData = UserData(
-            telegramName = user.getUserName(),
-            telegramId = user.getId(),
-            psnName = arguments[0])
-
-        store.createUserRecord(userData)
+        transaction {
+            User.new {
+                telegramName = user.getUserName()
+                telegramId = user.getId()
+                psnName = arguments[0]
+            }
+        }
 
         sendReply(absSender, chat, "Linking telegram @"+user.getUserName()+" with psn "+arguments[0])
     }
