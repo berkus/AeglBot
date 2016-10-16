394f2a3a7dc192d7e58ce5b3075216cc5bcb1984
Berkus Decker <berkus@madfire.net>
2016-10-16 07:35:41 +0300

    Implement /lfg command

--- bot/src/main/kotlin/org/aeriagloris/telegram/commands/LfgCommand.kt
+++ bot/src/main/kotlin/org/aeriagloris/telegram/commands/LfgCommand.kt
@@ -3,13 +3,14 @@ package org.aeriagloris.telegram.commands
 import org.telegram.telegrambots.TelegramApiException
 import org.telegram.telegrambots.api.methods.send.SendMessage
 import org.telegram.telegrambots.api.objects.Chat
-import org.telegram.telegrambots.api.objects.User
+import org.telegram.telegrambots.api.objects.User as TelegramUser
 import org.telegram.telegrambots.bots.AbsSender
 import org.telegram.telegrambots.logging.BotLogger
 import org.aeriagloris.persistence.JdbcStore
 import org.aeriagloris.telegram.services.ActivityIndex
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.aeriagloris.persistence.schema.*
+import org.jetbrains.exposed.sql.*
 
 import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
@@ -27,7 +28,7 @@ class LfgCommand(val store: JdbcStore)
             "(NB: times are in MSK timezone)", true)
     }
 
-    override fun execute(absSender: AbsSender, user: User, chat: Chat, arguments: Array<String>)
+    override fun execute(absSender: AbsSender, user: TelegramUser, chat: Chat, arguments: Array<String>)
     {
         if (arguments.size < 1) {
             usage(absSender, chat)
@@ -51,79 +52,50 @@ class LfgCommand(val store: JdbcStore)
         }
 
         transaction {
+            val dbUser = User.find { Users.telegramName eq user.getUserName() }.singleOrNull()
 
-///            val user = User.find { Users.telegramName eq user.getUserName() }.single()
-//            val user = Users.find { Users.name eq user.getUserName() }
-//
-//            val userId = store.lookupTelegramUserName()
-//
-//            if (userId == null) {
-//                sendReply(absSender, chat, "You need to link your PSN account first: use /psn command")
-//                return
-//            }
-//
-//            val lookedUpActivityId = store.lookupActivity(arguments[0])
-//
-//            if (lookedUpActivityId == null) {
-//                sendReply(absSender, chat, "Activity "+arguments[0]+" was not found.")
-//                return
-//            }
-//
-//            var time: String
-//            var tz: DateTimeZone
-//
-//            if (arguments[1].count() > 11) {
-//                time = arguments[1].substring(0, 11)
-//                tz = DateTimeZone.forID(arguments[1].substring(11))
-//            } else {
-//                time = arguments[1]
-//                tz = DateTimeZone.UTC
-//            }
-//
-//            val fmt = DateTimeFormat.forPattern("yyyy.MM.dd-HH:mm").withZone(tz);
-//            val start = fmt.parseDateTime("2016."+time)
-//            //sendReply(absSender, chat, "Defined moment to be "+start)
-//
-//            val activity = PlannedActivityData(
-//                authorId = userId,
-//                activityId = lookedUpActivityId,
-//                start = start,
-//                details = "")
-//
-//            val actId = store.createPlannedActivity(activity)
-//
-//            if (actId == null) {
-//                sendReply(absSender, chat, "Internal error: couldn't create activity")
-//                return
-//            }
-//
-//            // Add self as activity member
-//            val member = PlannedActivityMember()
-//
-//
-//            val userData = store.loadUserData(userId)
-//
-//            if (userData == null) {
-//                sendReply(absSender, chat, "Internal error: couldn't load user information")
-//                return
-//            }
-//
-//            val activityData = store.loadActivityData(actId)
-//
-//            if (activityData == null) {
-//                sendReply(absSender, chat, "Internal error: couldn't load activity information")
-//                return
-//            }
+            if (dbUser == null) {
+                sendReply(absSender, chat, "You need to link your PSN account first: use /psn command")
+            } else {
 
-        }
+                val act1 = ActivityIndex.map[ arguments[0] ]!!
+                val act = Activity
+                    .find { (Activities.name eq act1.first) and (Activities.mode eq act1.second) }
+                    .singleOrNull()
 
+                if (act == null) {
+                    sendReply(absSender, chat, "Activity "+arguments[0]+" was not found.")
+                } else {
+                    val fmt = if (arguments[1].count() > 11) {
+                            DateTimeFormat.forPattern("yyyy.MM.dd-HH:mmzzz")
+                        } else {
+                            DateTimeFormat.forPattern("yyyy.MM.dd-HH:mm").withZone(DateTimeZone.UTC)
+                        }
+
+                    val startTime = fmt.parseDateTime("2016."+arguments[1])
+                    sendReply(absSender, chat, "Defined moment to be "+startTime)
+        
+                    val plannedActivity = PlannedActivity.new {
+                        author = dbUser
+                        activity = act
+                        start = startTime
+                        details = arguments.drop(2).joinToString(" ")
+                    }
+
+                    PlannedActivityMember.new {
+                        this.user = dbUser
+                        this.activity = plannedActivity
+                    }
+
+                    sendReply(absSender, chat,
+                        dbUser.psnName + " (@" + dbUser.telegramName + ") is looking for "
+                        + act.name + " " + act.mode
+                        +" group "+formatStartTime(startTime)+"\n"
+                        + "Enter /join "+plannedActivity.id+" to join this group.")
+                }
+            }
 //"Today at 23:00 MSK (starts in 3 hours)" = formatStartTime(time)
-
-//        sendReply(absSender, chat,
-//            userData.psnName + " (@" + userData.telegramName + ") is looking for "
-//            + activityData.name + " " + activityData.mode
-//            +" group "+formatStartTime(start)+"\n"
-//            + "Enter /join "+actId+" to join this group.")
+        }
     }
 }
 
