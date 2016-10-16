6449e156bbcae319118ff80c0ce47020ae14515c
Berkus Decker <berkus@madfire.net>
2016-10-16 07:35:30 +0300

    Implement /join command

--- bot/src/main/kotlin/org/aeriagloris/telegram/commands/JoinCommand.kt
+++ bot/src/main/kotlin/org/aeriagloris/telegram/commands/JoinCommand.kt
@@ -3,14 +3,17 @@ package org.aeriagloris.telegram.commands
 import org.telegram.telegrambots.TelegramApiException
 import org.telegram.telegrambots.api.methods.send.SendMessage
 import org.telegram.telegrambots.api.objects.Chat
-import org.telegram.telegrambots.api.objects.User
+import org.telegram.telegrambots.api.objects.User as TelegramUser
 import org.telegram.telegrambots.bots.AbsSender
 import org.telegram.telegrambots.logging.BotLogger
 import org.aeriagloris.persistence.JdbcStore
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.aeriagloris.persistence.schema.*
+import org.jetbrains.exposed.sql.*
 
 class JoinCommand(val store: JdbcStore) : ExtendedCommand("join", "Join a fireteam from the list")
 {
-    override fun execute(absSender: AbsSender, user: User, chat: Chat, arguments: Array<String>)
+    override fun execute(absSender: AbsSender, user: TelegramUser, chat: Chat, arguments: Array<String>)
     {
         if (arguments.size != 1) {
             sendReply(absSender, chat, "To join a fireteam provide fireteam id\n"
@@ -18,8 +21,33 @@ class JoinCommand(val store: JdbcStore) : ExtendedCommand("join", "Join a firete
             return
         }
 
-        
+        transaction {
+            val dbUser = User.find { Users.telegramName eq user.getUserName() }.singleOrNull()
 
-        sendReply(absSender, chat, "You are joining - please provide fireteam id in arguments")
+            if (dbUser == null) {
+                sendReply(absSender, chat, "You need to link your PSN account first: use /psn command")
+            } else {
+
+                val planned = PlannedActivity
+                    .findById(arguments[0].toInt())
+
+                if (planned == null) {
+                    sendReply(absSender, chat, "Activity "+arguments[0]+" was not found.")
+                } else {
+
+                    PlannedActivityMember.new {
+                        this.user = dbUser
+                        this.activity = planned
+                    }
+
+                    sendReply(absSender, chat,
+                        dbUser.psnName + " (@" + dbUser.telegramName + ") is joining "
+                        + planned.activity.name + " " + planned.activity.mode
+                        +" group\n"
+                        +planned.members.joinToString { it.user.psnName }+" are going\n"
+                        + "Enter /join "+planned.id+" to join this group.")
+                }
+            }
+        }
     }
 }
