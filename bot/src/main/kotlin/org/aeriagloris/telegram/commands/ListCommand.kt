add0499e151882dc3e2c5d69b34aaa309574f30b
Berkus Decker <berkus@madfire.net>
2016-10-16 07:35:51 +0300

    [wip] /list command

--- bot/src/main/kotlin/org/aeriagloris/telegram/commands/ListCommand.kt
+++ bot/src/main/kotlin/org/aeriagloris/telegram/commands/ListCommand.kt
@@ -3,16 +3,31 @@ package org.aeriagloris.telegram.commands
 import org.telegram.telegrambots.TelegramApiException
 import org.telegram.telegrambots.api.methods.send.SendMessage
 import org.telegram.telegrambots.api.objects.Chat
-import org.telegram.telegrambots.api.objects.User
+import org.telegram.telegrambots.api.objects.User as TelegramUser
 import org.telegram.telegrambots.bots.AbsSender
 import org.telegram.telegrambots.logging.BotLogger
 import org.aeriagloris.persistence.JdbcStore
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.aeriagloris.persistence.schema.*
+import org.jetbrains.exposed.sql.*
 
 class ListCommand(val store: JdbcStore) : ExtendedCommand("list", "List current lfg/lfm")
 {
-    override fun execute(absSender: AbsSender, user: User, chat: Chat, arguments: Array<String>)
+    override fun execute(absSender: AbsSender, user: TelegramUser, chat: Chat, arguments: Array<String>)
     {
-        sendReply(absSender, chat, "This is a list\nblablab\nlong\nlist")
+        transaction {
+            val activs = PlannedActivity.all()
+            //.map {
+            //    "<b>"+it.id+"</b>: "+it.members.joinToString { it.user.psnName + " (@" + it.user.telegramName + ")" }+
+            //    " going to " + it.activity.name + " " + it.activity.mode +
+            //    " at <b>" + formatStartTime(it.start) + "</b>"
+            //}
+
+            sendReply(absSender, chat,
+                "Planned activities:\n"+activs
+                + "Enter /join <b>id</b> to join group.", true)
+
+        }
 
 // Sample output: (same output for List command)
 
