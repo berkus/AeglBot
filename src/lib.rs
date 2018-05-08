8887eaac62268d33f97f8e65101e08c6034726e6
Berkus Decker <berkus@madfire.net>
2018-05-08 12:37:47 +0300

    Start rust conversion

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+// Parallel Rust implementation of the bot
+// 
+// To make it usable it misses natty parsing lib implementation in rust
+// (yeah, i'd prefer native, although there are ways to use natty through jlink
+// or take python equivalent from https://dateparser.readthedocs.io/en/latest/)
