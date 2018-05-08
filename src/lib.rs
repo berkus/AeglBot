1371088b0ad5b968ecb73f8a21448b9c51d23c24
Berkus Decker <berkus@madfire.net>
2018-05-08 18:40:12 +0300

    Add library functions [wip]

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,23 @@
 // To make it usable it misses natty parsing lib implementation in rust
 // (yeah, i'd prefer native, although there are ways to use natty through jlink
 // or take python equivalent from https://dateparser.readthedocs.io/en/latest/)
+#[macro_use]
+extern crate diesel;
+extern crate chrono;
+extern crate dotenv;
+extern crate serde_json;
+
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+pub mod models;
+pub mod schema;
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
