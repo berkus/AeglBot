65b112c8a34f55b734671f1b677213f66ebf0f79
Berkus Decker <berkus+github@metta.systems>
2018-08-26 12:39:15 +0300

    Implement rudimentary activities

--- src/commands/activities_command.rs
+++ src/commands/activities_command.rs
@@ -1,6 +1,8 @@
 use crate::commands::BotCommand;
-use diesel::PgConnection;
-use telegram_bot::{self, CanReplySendMessage};
+use diesel::{prelude::*, PgConnection};
+use models::ActivityShortcut;
+use schema::activityshortcuts::dsl::*;
+use telegram_bot::{self, types::ParseMode, CanReplySendMessage};
 
 pub struct ActivitiesCommand;
 
@@ -16,27 +18,36 @@ impl BotCommand for ActivitiesCommand {
     fn execute(
         api: &telegram_bot::Api,
         message: &telegram_bot::Message,
-        command: Option<String>,
-        name: Option<String>,
+        _command: Option<String>,
+        _unused: Option<String>,
         connection: &PgConnection,
     ) {
-        api.spawn(message.text_reply("not implemented yet"));
+        let games = activityshortcuts
+            .select(game)
+            .distinct()
+            .order(game.asc())
+            .load::<String>(connection)
+            .expect("TEMP loading @FIXME");
 
-        // let games = activitiesshortcuts
-        //     .filter(telegram_name.eq(&username)) // @todo Fix with tg-id
-        //     .limit(1)
-        //     .load::<Guardian>(connection);
+        let mut text = "Activities: use a short name:\n".to_owned();
 
-        // var text = "Activities: use a short name:\n";
-        // val games = ActivityShortcuts.slice(ActivityShortcuts.game).selectAll().withDistinct().toList()
-        //     .map { game -> game[ActivityShortcuts.game] }.sorted()
-
-        // for (game in games) {
-        //     text += "*** <b>${game}</b>:\n" +
-        //         ActivityShortcut.find { ActivityShortcuts.game eq game }.toList().sortedBy { ActivityShortcuts.name }.map { act ->
-        //             "<b>${act.name}</b>\t${act.link.formatName()}"
-        //         }.joinToString("\n") + "\n"
-        // }
-        // api.spawn(message.text_reply("not implemented yet"));
+        for game_name in games.into_iter() {
+            text += &format!("*** <b>{0}</b>:\n", game_name);
+            let shortcuts = activityshortcuts
+                .filter(game.eq(game_name))
+                .order(name.asc())
+                .load::<ActivityShortcut>(connection)
+                .expect("TEMP loading @FIXME");
+            for shortcut in shortcuts.into_iter() {
+                text += &format!(
+                    "<b>{name}</b>\t{link}\n",
+                    name = shortcut.name,
+                    link = shortcut.link
+                );
+                // shortcut.link.formatName()
+            }
+            text += "\n";
+        }
+        api.spawn(message.text_reply(text).parse_mode(ParseMode::Html));
     }
 }
