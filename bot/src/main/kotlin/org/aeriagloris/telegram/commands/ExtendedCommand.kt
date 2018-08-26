d9d8c85a67129b6ea390f11d6c1d26f88dd6913b
Berkus Decker <berkus+github@metta.systems>
2018-08-26 17:10:01 +0300

    cleanup

--- bot/src/main/kotlin/org/aeriagloris/telegram/commands/ExtendedCommand.kt
+++ bot/src/main/kotlin/org/aeriagloris/telegram/commands/ExtendedCommand.kt
@@ -1,17 +1,3 @@
-package org.aeriagloris.telegram.commands
-
-import org.telegram.telegrambots.exceptions.TelegramApiException
-import org.telegram.telegrambots.api.methods.send.SendMessage
-import org.telegram.telegrambots.api.objects.Chat
-import org.telegram.telegrambots.bots.AbsSender
-import org.telegram.telegrambots.bots.commandbot.commands.BotCommand
-import org.telegram.telegrambots.logging.BotLogger
-import java.util.concurrent.TimeUnit
-import org.joda.time.DateTime
-import org.joda.time.format.DateTimeFormat
-import com.joestelmach.natty.*
-import java.util.TimeZone
-
 fun sendReplyMessage(absSender: AbsSender, chatId: Long, message: String, isHtml: Boolean = false) {
     val answer = SendMessage()
     answer.setChatId(chatId)
@@ -29,79 +15,24 @@ fun sendReplyMessage(absSender: AbsSender, chatId: Long, message: String, isHtml
     }
 }
 
-abstract class ExtendedCommand(tag: String, text: String) : BotCommand(tag, text)
-{
-    fun sendMessage(absSender: AbsSender, chat: Chat, message: SendMessage) {
-        message.setChatId(chat.id)
-        message.disableNotification()
-        message.disableWebPagePreview()
+fun sendMessage(absSender: AbsSender, chat: Chat, message: SendMessage) {
+    message.setChatId(chat.id)
+    message.disableNotification()
+    message.disableWebPagePreview()
 
-        try {
-            absSender.sendMessage(message)
-        } catch (e: TelegramApiException) {
-            BotLogger.error("COMMAND", e)
-        }
+    try {
+        absSender.sendMessage(message)
+    } catch (e: TelegramApiException) {
+        BotLogger.error("COMMAND", e)
     }
+}
 
-    fun sendReply(absSender: AbsSender, chat: Chat, message: String, isHtml: Boolean = false) {
-        sendReplyMessage(absSender, chat.id, message, isHtml)
-    }
+fun sendReply(absSender: AbsSender, chat: Chat, message: String, isHtml: Boolean = false) {
+    sendReplyMessage(absSender, chat.id, message, isHtml)
+}
 
-    fun parseTimeSpec(timespec: String): DateTime {
-        val parser = Parser(TimeZone.getTimeZone("Europe/Moscow")) // @todo bot.timezone
-        val groups = parser.parse(timespec)
-        return DateTime(groups[0].dates[0])
-    }
-
-    fun timeDiffString(duration: Long): String {
-        val times = arrayOf(
-            TimeUnit.DAYS.toMillis(365),
-            TimeUnit.DAYS.toMillis(30),
-            TimeUnit.DAYS.toMillis(1),
-            TimeUnit.HOURS.toMillis(1),
-            TimeUnit.MINUTES.toMillis(1)
-        )
-        val timesString = arrayOf("year","month","day","hour","minute")
-
-        var dur = Math.abs(duration)
-        val res = times.zip(timesString).map { item ->
-            val (current, timesStr) = item
-            val temp = dur / current
-            if (temp > 0) {
-                dur -= temp * current
-                temp.toString() + " " + timesStr + if (temp != 1L) { "s" } else { "" }
-            } else {
-                ""
-            }
-        }.joinToString(" ").trim()
-
-        if ("".equals(res)) {
-            return "just now"
-        }
-        else {
-            if (duration > 0) {
-                return "in " + res
-            }
-            else {
-                return res + " ago"
-            }
-        }
-    }
-
-    // "Today at 23:00 (starts in 3 hours)"
-    // @todo make static
-    fun formatStartTime(time: DateTime): String {
-        val prefix = if (time.withTime(0,0,0,0) == DateTime.now().withTime(0,0,0,0)) {
-            "Today"
-        } else {
-            "on " + DateTimeFormat.forStyle("S-").print(time)
-        }
-
-        val prefix2 = DateTimeFormat.forStyle("-S").print(time)
-
-        val timeDiff = time.getMillis() - DateTime.now().getMillis()
-        val infixStr = if (timeDiff <= 0) { "started" } else { "starts" }
-
-        return "${prefix} at ${prefix2} (${infixStr} ${timeDiffString(timeDiff)})"
-    }
+fun parseTimeSpec(timespec: String): DateTime {
+    val parser = Parser(TimeZone.getTimeZone("Europe/Moscow")) // @todo bot.timezone
+    val groups = parser.parse(timespec)
+    return DateTime(groups[0].dates[0])
 }
