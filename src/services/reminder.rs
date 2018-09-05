d5c94b56e7362ef6984e1d6369aa33e5bdae783d
Berkus Decker <berkus+github@metta.systems>
2018-09-05 10:29:08 +0300

    Show upcoming activities

--- src/services/reminder.rs
+++ src/services/reminder.rs
@@ -1,30 +1,18 @@
-// class Reminder(val store: JdbcStore)
-// {
-//     // Check for upcoming events and remind to specified LFG chat
-//     fun check(chatId: String)
-//     {
-//         transaction {
-//             logger.addLogger(Slf4jSqlLogger())
+// val minutesAgo = DateTime.now(DateTimeZone.forID("Europe/Moscow")).minusMinutes(15)
+// val objs = PlannedActivity.find {
+//         PlannedActivities.start greaterEq minutesAgo
+//     }.toList().sortedBy { it.start }.map { act ->
+//         "<b>"+act.id+"</b>: <b>"+act.activity.formatName()+"</b>\n" +
+//             act.detailsFormatted() +
+//             act.membersFormattedColumn() + "\n" +
+//             //"<b>" + formatStartTime(act.start) + "</b>\n" +
+//             act.joinPrompt() + "\n"
+//     }.joinToString("\n")
 
-//             val minutesAgo = DateTime.now(DateTimeZone.forID("Europe/Moscow")).minusMinutes(15)
-//             val objs = PlannedActivity.find {
-//                     PlannedActivities.start greaterEq minutesAgo
-//                 }.toList().sortedBy { it.start }.map { act ->
-//                     "<b>"+act.id+"</b>: <b>"+act.activity.formatName()+"</b>\n" +
-//                         act.detailsFormatted() +
-//                         act.membersFormattedColumn() + "\n" +
-//                         //"<b>" + formatStartTime(act.start) + "</b>\n" +
-//                         act.joinPrompt() + "\n"
-//                 }.joinToString("\n")
-
-//             if ("".equals(objs)) {
-//                 //sendReply(absSender, chat, "No activities planned, add something with /lfg")
-//             } else {
-//                 print(objs)
-//                 //sendReply(absSender, chat, "Planned activities:\n\n" + objs, true)
-//             }
-//         }
-//     }
+// if ("".equals(objs)) {
+// } else {
+//     print(objs)
+//     //sendReply(absSender, chat, "Planned activities:\n\n" + objs, true)
 // }
 use chrono::NaiveDateTime;
 use crate::commands::send_html_message;
@@ -36,9 +24,38 @@ use futures::Future;
 use telebot::{functions::*, RcBot};
 
 pub fn check(
-    _bot: &RcBot,
-    _chat_id: telebot::objects::Integer,
-    _connection: &DbConnection,
+    bot: &RcBot,
+    chat_id: telebot::objects::Integer,
+    connection: &DbConnection,
 ) -> Result<(), Error> {
+    use schema::plannedactivities::dsl::*;
+
+    let reference = reference_date();
+
+    let upcoming_events = plannedactivities
+        .filter(start.ge(now - 60_i32.minutes())) // FIXME this will sort based on DB local TZ
+        .order(start.asc())
+        .load::<PlannedActivity>(connection)
+        .expect("TEMP loading @FIXME");
+
+    let upcoming_events: Vec<&PlannedActivity> = upcoming_events
+        .iter()
+        .filter(|event| match (event.start - reference).num_minutes() {
+            60 | 30 | 15 | 5 => true,
+            _ => false,
+        }).collect();
+
+    if upcoming_events.is_empty() {
+        return Ok(());
+    }
+
+    let text = upcoming_events
+        .into_iter()
+        .fold("Activities starting soon:\n\n".to_owned(), |acc, event| {
+            acc + &format!("{}\n\n", event.display(connection, None))
+        });
+
+    send_html_message(bot, chat_id, text);
+
     Ok(())
 }
