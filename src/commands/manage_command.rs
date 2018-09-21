2fb3af8dd5242bdcfe4d35ecaf99a244de447625
Berkus Decker <berkus+github@metta.systems>
2018-09-22 00:58:43 +0300

    Implement /manage subcommands skeleton

--- src/commands/manage_command.rs
+++ src/commands/manage_command.rs
@@ -1,17 +1,23 @@
 //☐ manage admins (superadmin can add/remove admins, admins cannot add more admins?)
 //☐ `/manage` catch-all command for these things
-use crate::{Bot, BotCommand, DbConnection, commands::admin_check};
+use crate::{commands::admin_check, Bot, BotCommand, DbConnection};
 
 pub struct ManageCommand;
 struct ListAdminsSubcommand;
 struct AddAdminSubcommand;
 struct RemoveAdminSubcommand;
 
-impl ManageCommand {
-    pub fn new() -> Box<Self> {
-        Box::new(ManageCommand)
-    }
+macro_rules! ctor {
+    ($name:ident) => (impl $name {
+        pub fn new() -> Box<Self> {
+            Box::new($name)
+        }
+    })
+}
 
+ctor!(ManageCommand);
+
+impl ManageCommand {
     fn usage(bot: &Bot, message: &telebot::objects::Message) {
         bot.send_plain_reply(
             &message,
@@ -40,8 +46,8 @@ impl BotCommand for ManageCommand {
         &self,
         bot: &Bot,
         message: &telebot::objects::Message,
+        _command: Option<String>,
         args: Option<String>,
-        _name: Option<String>,
     ) {
         let connection = bot.connection();
         let admin = admin_check(bot, message, &connection);
@@ -57,16 +63,34 @@ impl BotCommand for ManageCommand {
         }
 
         // Split args in two:
-        // activity spec,
-        // and timespec
+        // subcommand,
+        // and optional guardian id
         let args = args.unwrap();
         let args: Vec<&str> = args.splitn(2, ' ').collect();
 
-        if args.len() < 2 {
+        if args.len() < 1 {
             return ManageCommand::usage(bot, &message);
         }
 
+        let subcommand = args[0];
+        let args = if args.len() > 1 { Some(args[1].to_string()) } else { None };
+
         info!("{:?}", args);
+
+        match subcommand {
+            "list-admins" => {
+                ListAdminsSubcommand::new().execute(bot, message, Some("list-admins".into()), None)
+            }
+            "add-admin" => {
+                AddAdminSubcommand::new().execute(bot, message, Some("add-admin".into()), args)
+            }
+            "remove-admin" => {
+                RemoveAdminSubcommand::new().execute(bot, message, Some("remove-admin".into()), args)
+            }
+            &_ => {
+                bot.send_plain_reply(&message, "Unknown management command".into());
+            }
+        }
 
         // Need to invent some sort of match string format for matching subcommands
         // Some are `/command subcommand [args]`, some are `/command arg subcommand args` etc.
@@ -77,9 +101,10 @@ impl BotCommand for ManageCommand {
         //        } else if match_subcommand(message, AddAdminSubcommand) {
         //            return AddAdminSubcommand::execute();
         //        }
-        bot.send_plain_reply(&message, "Not implemented".into());
     }
 }
+
+ctor!(ListAdminsSubcommand);
 
 impl BotCommand for ListAdminsSubcommand {
     fn prefix(&self) -> &'static str {
@@ -95,10 +120,13 @@ impl BotCommand for ListAdminsSubcommand {
         bot: &Bot,
         message: &telebot::objects::Message,
         _command: Option<String>,
-        _name: Option<String>,
+        args: Option<String>,
     ) {
+        bot.send_plain_reply(&message, "List-admins command".into());
     }
 }
+
+ctor!(AddAdminSubcommand);
 
 impl BotCommand for AddAdminSubcommand {
     fn prefix(&self) -> &'static str {
@@ -114,10 +142,13 @@ impl BotCommand for AddAdminSubcommand {
         bot: &Bot,
         message: &telebot::objects::Message,
         _command: Option<String>,
-        _name: Option<String>,
+        args: Option<String>,
     ) {
+        bot.send_plain_reply(&message, "Add-admin command".into());
     }
 }
+
+ctor!(RemoveAdminSubcommand);
 
 impl BotCommand for RemoveAdminSubcommand {
     fn prefix(&self) -> &'static str {
@@ -133,7 +164,8 @@ impl BotCommand for RemoveAdminSubcommand {
         bot: &Bot,
         message: &telebot::objects::Message,
         _command: Option<String>,
-        _name: Option<String>,
+        args: Option<String>,
     ) {
+        bot.send_plain_reply(&message, "Remove-admin command".into());
     }
 }
