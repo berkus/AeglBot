6e5b008e646a7aaa3b1f75f8fa859b7e1c182f40
Berkus Decker <berkus+github@metta.systems>
2023-09-02 17:47:39 +0300

    fix: Drop obsolete feature

--- src/bin/bot.rs
+++ src/bin/bot.rs
@@ -3,7 +3,6 @@
 // To make it usable it misses natty parsing lib implementation in rust
 // (yeah, i'd prefer native, although there are ways to use natty through jlink
 // or take python equivalent from https://dateparser.readthedocs.io/en/latest/)
-#![feature(box_syntax)]
 #![feature(associated_type_bounds)]
 
 use {
