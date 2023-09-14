b61b2fdf21d6a5243ebb62c6ba3e063cf613d868
Berkus Decker <berkus+github@metta.systems>
2023-09-15 00:47:30 +0300

    refactor: 📦 Remove duplicate code

--- src/commands/list_command.rs
+++ src/commands/list_command.rs
@@ -2,11 +2,9 @@ use {
     crate::{
         bot_actor::{ActorUpdateMessage, Format, Notify, SendMessageReply},
         commands::{match_command, validate_username},
-        datetime::nowtz,
         models::PlannedActivity,
         BotCommand,
     },
-    diesel::{self, dsl::IntervalDsl, prelude::*},
     riker::actors::Tell,
 };
 
@@ -39,25 +37,22 @@ impl Receive<ActorUpdateMessage> for ListCommand {
 
     fn receive(&mut self, _ctx: &Context<Self::Msg>, message: ActorUpdateMessage, _sender: Sender) {
         if let (Some(_), _) = match_command(message.update.text(), Self::prefix(), &self.bot_name) {
-            use crate::schema::plannedactivities::dsl::*;
-
             let connection = self.connection();
 
-            let upcoming_events = plannedactivities
-                .filter(start.ge(nowtz() - 60_i32.minutes()))
-                .order(start.asc())
-                .load::<PlannedActivity>(&connection)
-                .expect("TEMP loading @FIXME");
-
-            if upcoming_events.is_empty() {
-                return self.send_reply(
-                    &message,
-                    "No activities planned, add something with /lfg",
-                    Format::Plain,
-                );
-            }
-
             if let Some(guardian) = validate_username(&self.bot_ref, &message, &connection) {
+                // let count = self.activity(connection).max_fireteam_size as usize
+                //     - self.members_count(connection);
+
+                let upcoming_events = PlannedActivity::upcoming_activities(&connection);
+
+                if upcoming_events.is_empty() {
+                    return self.send_reply(
+                        &message,
+                        "No activities planned, add something with /lfg",
+                        Format::Plain,
+                    );
+                }
+
                 let text = upcoming_events.iter().fold(
                     "Planned activities:\n\n".to_owned(),
                     |acc, event| {
--- src/services/reminder.rs
+++ src/services/reminder.rs
@@ -1,27 +1,20 @@
 use {
     crate::{
         bot_actor::{BotActorMsg, Format, Notify, SendMessage},
-        datetime::{nowtz, reference_date},
+        datetime::reference_date,
         models::PlannedActivity,
         BotConnection,
     },
-    diesel::{self, dsl::IntervalDsl, prelude::*},
     riker::{actor::Tell, actors::ActorRef},
     teloxide::types::ChatId,
 };
 
 pub fn check(bot: ActorRef<BotActorMsg>, connection: BotConnection, chat_id: ChatId) {
-    use crate::schema::plannedactivities::dsl::*;
-
     // log::info!("reminder check at {}", reference_date());
 
     let reference = reference_date();
 
-    let upcoming_events = plannedactivities
-        .filter(start.ge(nowtz() - 60_i32.minutes()))
-        .order(start.asc())
-        .load::<PlannedActivity>(&connection)
-        .expect("TEMP loading @FIXME");
+    let upcoming_events = PlannedActivity::upcoming_activities(&connection);
 
     let upcoming_events: Vec<&PlannedActivity> = upcoming_events
         .iter()
