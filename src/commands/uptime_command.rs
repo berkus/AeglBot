bc2b7e6be70565bf6f2685b3be495d2d4f56016f
Berkus Decker <berkus+github@metta.systems>
2025-08-02 17:24:14 +0300

    fix: 💄Better formatting of uptime

--- src/commands/uptime_command.rs
+++ src/commands/uptime_command.rs
@@ -18,7 +18,7 @@ fn get_process_info() -> String {
         let stat = process.stat().unwrap();
         let page_size = procfs::page_size();
         format!(
-            "🧵 {thn} threads\n📃 {vmb} bytes ({vmp} pages) virtual memory\n📃 {rmb} bytes ({rmp} pages) resident memory",
+            "- 🧵 {thn} threads\n- 📃 {vmb} bytes ({vmp} pages) virtual memory\n- 📃 {rmb} bytes ({rmp} pages) resident memory",
             thn = stat.num_threads,
             vmb = stat.vsize.separate_with_commas(),
             vmp = (stat.vsize / page_size).separate_with_commas(),
@@ -26,13 +26,13 @@ fn get_process_info() -> String {
             rmb = (stat.rss * page_size).separate_with_commas(),
         )
     } else {
-        "Couldn't access process information".to_string()
+        "- Couldn't access process information".to_string()
     }
 }
 
 #[cfg(not(target_os = "linux"))]
 fn get_process_info() -> String {
-    "Process info only available on Linux hosts.".to_string()
+    "- Process info only available on Linux hosts.".to_string()
 }
 
 impl BotCommand for UptimeCommand {
@@ -51,7 +51,7 @@ impl Receive<ActorUpdateMessage> for UptimeCommand {
     fn receive(&mut self, _ctx: &Context<Self::Msg>, msg: ActorUpdateMessage, _sender: Sender) {
         if let (Some(_), _) = match_command(msg.update.text(), Self::prefix(), &self.bot_name) {
             let uptime = crate::datetime::format_uptime();
-            let message = format!("- ⏰ Started {uptime}\n- {}", get_process_info());
+            let message = format!("- ⏰ Started {uptime}\n{}", get_process_info());
             self.bot_ref.tell(
                 SendMessageReply(message, msg, Format::Plain, Notify::Off),
                 None,
