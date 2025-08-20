7b7a23797831ba09c7ca44b52f91779fbeda5e37
Berkus Decker <berkus+github@metta.systems>
2025-08-20 15:03:43 +0300

    👽 Update Edit command

--- bot/src/commands/edit_command.rs
+++ bot/src/commands/edit_command.rs
@@ -1,180 +1,160 @@
 use {
     crate::{
-        BotCommand,
-        bot_actor::{ActorUpdateMessage, Format, Notify, SendMessageReply},
+        actors::bot_actor::ActorUpdateMessage,
         commands::{match_command, validate_username},
-        models::{ActivityShortcut, PlannedActivity},
     },
-    chrono::{Duration, prelude::*},
-    chrono_english::{Dialect, parse_date_string},
-    chrono_tz::Europe::Moscow,
-    diesel_derives_traits::Model,
-    riker::actors::Tell,
+    chrono::{prelude::*, Duration},
+    entity::{activityshortcuts, plannedactivities},
+    kameo::message::Context,
     libbot::datetime::reference_date,
+    sea_orm::{ActiveModelTrait, ColumnTrait, EntityTrait, QueryFilter, Set},
 };
 
-command_actor!(EditCommand, [ActorUpdateMessage]);
+command_actor!(EditCommand, "edit", "Edit existing activity");
 
-impl EditCommand {
-    fn send_reply<S>(&self, message: &ActorUpdateMessage, reply: S)
-    where
-        S: Into<String>,
-    {
-        self.bot_ref.tell(
-            SendMessageReply(reply.into(), message.clone(), Format::Plain, Notify::Off),
-            None,
-        );
-    }
+impl Message<ActorUpdateMessage> for EditCommand {
+    type Reply = ();
 
-    fn usage(&self, message: &ActorUpdateMessage) {
-        self.send_reply(
-            message,
-            "Usage:
-
-ActivityIDs are available from output of /list command.
-
-/edit ActivityID time <new time>
-    Change activity time to a new one. Accepted time spec
-    is the same as in /lfg command.
-
-/edit ActivityID details <free form details description>
-    Replaces old /details command.
-    To update activity details enter text,
-    to delete details use `delete` instead of text.
-
-/edit ActivityID activity <new activity shortcut>
-    Change type of activity, list of shortcuts
-    is available from output of /activities command",
-        );
-    }
-}
-
-impl BotCommand for EditCommand {
-    fn prefix() -> &'static str {
-        "/edit"
-    }
-
-    fn description() -> &'static str {
-        "Edit existing activity"
-    }
-}
-
-impl Receive<ActorUpdateMessage> for EditCommand {
-    type Msg = EditCommandMsg;
-
-    fn receive(&mut self, _ctx: &Context<Self::Msg>, message: ActorUpdateMessage, _sender: Sender) {
+    async fn handle(
+        &mut self,
+        message: ActorUpdateMessage,
+        _ctx: &mut Context<Self, Self::Reply>,
+    ) -> Self::Reply {
         if let (Some(_), args) =
             match_command(message.update.text(), Self::prefix(), &self.bot_name)
         {
-            let connection = self.connection();
-
             if args.is_none() {
-                return self.usage(&message);
+                return self.usage(&message).await;
             }
             let args = args.unwrap();
 
             let args: Vec<_> = args.splitn(3, ' ').collect();
             if args.len() != 3 {
-                return self.usage(&message);
+                return self.usage(&message).await;
             }
 
-            if validate_username(&self.bot_ref, &message, &connection).is_some() {
+            let connection = self.connection();
+
+            if validate_username(&self.bot_ref, &message, connection)
+                .await
+                .is_some()
+            {
                 let id = args[0].parse::<i32>();
                 if id.is_err() {
-                    return self.send_reply(&message, "ActivityID must be a number");
+                    return self
+                        .send_reply(&message, "❌ Activity ID must be a number")
+                        .await;
                 }
                 let id = id.unwrap();
 
-                let planned =
-                    PlannedActivity::find_one(&connection, &id).expect("Failed to run SQL");
+                let planned = plannedactivities::Entity::find_by_id(id)
+                    .one(connection)
+                    .await
+                    .expect("❌ Failed to run SQL");
 
                 if planned.is_none() {
-                    return self.send_reply(&message, format!("Activity {} was not found.", id));
+                    return self
+                        .send_reply(&message, format!("❌ Activity {} was not found.", id))
+                        .await;
                 }
-                let mut planned = planned.unwrap();
+                let planned = planned.unwrap();
 
                 if planned.start < reference_date() - Duration::hours(1) {
-                    return self.send_reply(&message, "You can not edit past activities.");
+                    return self
+                        .send_reply(&message, "❌ You can not edit past activities.")
+                        .await;
                 }
 
                 match args[1] {
                     "time" => {
                         let timespec = args[2];
-                        let start_time = parse_date_string(
-                            timespec,
-                            Local::now().with_timezone(&Moscow),
-                            Dialect::Uk,
-                        );
-                        // @todo Honor TELEGRAM_BOT_TIMEZONE envvar
-
-                        if start_time.is_err() {
-                            return self.send_reply(
-                                &message,
-                                format!("Failed to parse time {}", timespec),
-                            );
-                        }
-
-                        // ...then convert back to UTC.
-                        let start_time = start_time.unwrap().with_timezone(&Utc);
+                        let start_time = match libbot::datetime::parse_time_spec(timespec) {
+                            Ok(start) => start, //.and_utc(),
+                            Err(_) => {
+                                return self
+                                    .send_reply(
+                                        &message,
+                                        format!("❌ Failed to parse time {}", timespec),
+                                    )
+                                    .await;
+                            }
+                        };
 
                         log::info!("...parsed `{:?}`", start_time);
 
-                        if planned.start < reference_date() - Duration::hours(1) {
-                            return self.send_reply(
-                                &message,
-                                "You can not set activity time in the past.",
-                            );
+                        if start_time < reference_date() - Duration::hours(1) {
+                            return self
+                                .send_reply(
+                                    &message,
+                                    "❌ You can not set activity time in the past.",
+                                )
+                                .await;
                         }
 
-                        planned.start = start_time;
+                        let mut planned: plannedactivities::ActiveModel = planned.into();
+                        let offset = start_time.offset().fix();
+                        planned.start = Set(start_time.with_timezone(&offset));
 
-                        if planned.save(&connection).is_err() {
-                            return self.send_reply(&message, "Failed to update start time.");
+                        if planned.update(connection).await.is_err() {
+                            return self
+                                .send_reply(&message, "❌ Failed to update start time.")
+                                .await;
                         }
 
-                        self.send_reply(&message, "Start time updated.");
+                        self.send_reply(&message, "✅ Start time updated.").await;
                     }
                     "details" => {
                         let description = args[2];
-                        planned.details = if description == "delete" {
+                        let mut planned: plannedactivities::ActiveModel = planned.into();
+                        planned.details = Set(if description == "delete" {
                             Some(String::new())
                         } else {
                             Some(description.to_string())
-                        };
-                        if planned.save(&connection).is_err() {
-                            return self.send_reply(&message, "Failed to update details.");
+                        });
+
+                        if planned.update(connection).await.is_err() {
+                            return self
+                                .send_reply(&message, "❌ Failed to update details.")
+                                .await;
                         }
 
-                        self.send_reply(&message, "Details updated.");
+                        self.send_reply(&message, "✅ Details updated.").await;
                     }
                     "activity" => {
                         let activity = args[2];
 
-                        let act = ActivityShortcut::find_one_by_name(&connection, activity)
-                            .expect("Failed to load Activity shortcut");
+                        let act = activityshortcuts::Entity::find()
+                            .filter(activityshortcuts::Column::Name.eq(activity))
+                            .one(connection)
+                            .await
+                            .expect("❌ Failed to load Activity shortcut");
 
                         if act.is_none() {
-                            self.send_reply(
-                                &message,
-                                format!(
-                                    "Activity {} was not found. Use /activities for a list.",
-                                    activity
-                                ),
-                            );
+                            return self
+                                .send_reply(
+                                    &message,
+                                    format!(
+                                        "❌ Activity {} was not found. Use /activities for a list.",
+                                        activity
+                                    ),
+                                )
+                                .await;
                         }
 
                         let act = act.unwrap();
+                        let mut planned: plannedactivities::ActiveModel = planned.into();
+                        planned.activity_id = Set(act.link);
 
-                        planned.activity_id = act.link;
-
-                        if planned.save(&connection).is_err() {
-                            return self.send_reply(&message, "Failed to update activity.");
+                        if planned.update(connection).await.is_err() {
+                            return self
+                                .send_reply(&message, "❌ Failed to update activity type.")
+                                .await;
                         }
 
-                        self.send_reply(&message, "Activity updated.");
+                        self.send_reply(&message, "✅ Activity type updated.").await;
                     }
-                    x => {
-                        self.send_reply(&message, format!("Unknown attribute {}", x));
+                    _ => {
+                        self.usage(&message).await;
                     }
                 }
             }
