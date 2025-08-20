ecd6a1d427aa3455c2d82751b844abbc7c28bfbd
Berkus Decker <berkus+github@metta.systems>
2025-08-20 15:21:51 +0300

    👽 Update D2Week command

--- bot/src/commands/d2week_command.rs
+++ bot/src/commands/d2week_command.rs
@@ -1,34 +1,25 @@
 use {
     crate::{
-        actors::bot_actor::{ActorUpdateMessage, Format, Notify, SendMessageReply},
+        actors::bot_actor::{ActorUpdateMessage, Format},
         commands::match_command,
-        BotCommand,
     },
+    kameo::message::Context,
     libbot::services::destiny_schedule::this_week_in_d2,
-    riker::actors::Tell,
 };
 
-command_actor!(D2weekCommand, [ActorUpdateMessage]);
+command_actor!(D2weekCommand, "d2week", "Show current Destiny 2 week");
 
-impl BotCommand for D2weekCommand {
-    fn prefix() -> &'static str {
-        "/d2week"
-    }
+impl Message<ActorUpdateMessage> for D2weekCommand {
+    type Reply = ();
 
-    fn description() -> &'static str {
-        "Show current Destiny 2 week"
-    }
-}
-
-impl Receive<ActorUpdateMessage> for D2weekCommand {
-    type Msg = D2weekCommandMsg;
-
-    fn receive(&mut self, _ctx: &Context<Self::Msg>, msg: ActorUpdateMessage, _sender: Sender) {
-        if let (Some(_), _) = match_command(msg.update.text(), Self::prefix(), &self.bot_name) {
-            self.bot_ref.tell(
-                SendMessageReply(this_week_in_d2(), msg, Format::Markdown, Notify::Off),
-                None,
-            );
+    async fn handle(
+        &mut self,
+        message: ActorUpdateMessage,
+        _ctx: &mut Context<Self, Self::Reply>,
+    ) -> Self::Reply {
+        if let (Some(_), _) = match_command(message.update.text(), Self::prefix(), &self.bot_name) {
+            self.send_reply_with_format(&message, this_week_in_d2(), Format::Markdown)
+                .await;
         }
     }
 }
