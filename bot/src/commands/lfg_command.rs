3ac7caf5725e05527cfc5e3c18e8babe672f2b95
Berkus Decker <berkus+github@metta.systems>
2025-08-20 15:04:15 +0300

    👽 Update LFG command

--- bot/src/commands/lfg_command.rs
+++ bot/src/commands/lfg_command.rs
@@ -1,64 +1,32 @@
 use {
     crate::{
-        actors::bot_actor::{ActorUpdateMessage, Format, Notify, SendMessageReply},
+        actors::bot_actor::ActorUpdateMessage,
         commands::{match_command, validate_username},
-        models::{Activity, ActivityShortcut, NewPlannedActivity, NewPlannedActivityMember},
+        render_template_or_err,
     },
-    chrono::prelude::*,
-    chrono_english::{parse_date_string, Dialect},
-    chrono_tz::Europe::Moscow,
-    diesel::{self, prelude::*},
-    diesel_derives_traits::{Model, NewModel},
+    entity::{activities, activityshortcuts, plannedactivities, plannedactivitymembers},
+    kameo::message::Context,
     libbot::datetime::{format_start_time, reference_date},
-    riker::actors::Tell,
+    sea_orm::{ActiveModelTrait, ColumnTrait, EntityTrait, QueryFilter, Set},
 };
 
-command_actor!(LfgCommand, [ActorUpdateMessage]);
+command_actor!(LfgCommand, "lfg", "Create a new Looking For Group event");
 
-impl LfgCommand {
-    fn send_reply<S>(&self, message: &ActorUpdateMessage, reply: S, format: Format)
-    where
-        S: Into<String>,
-    {
-        self.bot_ref.tell(
-            SendMessageReply(reply.into(), message.clone(), format, Notify::Off),
-            None,
-        );
-    }
+impl Message<ActorUpdateMessage> for LfgCommand {
+    type Reply = ();
 
-    fn usage(&self, message: &ActorUpdateMessage) {
-        self.send_reply(
-            message,
-            "LFG usage: /lfg <b>activity</b> YYYY-MM-DD HH:MM
-For a list of activity codes: /activities
-Example: /lfg kf 2018-09-10 23:00
-Times are in Moscow (MSK) timezone.",
-            Format::Html,
-        );
-    }
-}
-
-impl BotCommand for LfgCommand {
-    fn prefix() -> &'static str {
-        "/lfg"
-    }
-
-    fn description() -> &'static str {
-        "Create a new Looking For Group event"
-    }
-}
-
-impl Receive<ActorUpdateMessage> for LfgCommand {
-    type Msg = LfgCommandMsg;
-
-    fn receive(&mut self, _ctx: &Context<Self::Msg>, message: ActorUpdateMessage, _sender: Sender) {
+    async fn handle(
+        &mut self,
+        message: ActorUpdateMessage,
+        _ctx: &mut Context<Self, Self::Reply>,
+    ) -> Self::Reply {
         if let (Some(_), args) =
             match_command(message.update.text(), Self::prefix(), &self.bot_name)
         {
             log::info!("args are {:?}", args);
 
             if args.is_none() {
-                return self.usage(&message);
+                return self.usage(&message).await;
             }
 
             // Split args in two:
@@ -67,96 +35,100 @@ impl Receive<ActorUpdateMessage> for LfgCommand {
             let args = args.unwrap();
             let args: Vec<&str> = args.splitn(2, ' ').collect();
 
-            if args.len() < 2 {
-                return self.usage(&message);
+            if args.len() != 2 {
+                return self.usage(&message).await;
             }
 
             let activity = args[0];
             let timespec = args[1];
-            let connection = self.connection();
 
             log::info!("Adding activity `{}` at `{}`", &activity, &timespec);
 
-            if let Some(guardian) = validate_username(&self.bot_ref, &message, &connection) {
-                let act = ActivityShortcut::find_one_by_name(&connection, activity)
-                    .expect("Failed to load Activity shortcut");
+            let connection = self.connection();
+
+            if let Some(guardian) = validate_username(&self.bot_ref, &message, connection).await {
+                let act = activityshortcuts::Entity::find()
+                    .filter(activityshortcuts::Column::Name.eq(activity))
+                    .one(connection)
+                    .await
+                    .expect("❌ Failed to load Activity shortcut");
 
                 if act.is_none() {
-                    return self.send_reply(
-                        &message,
-                        format!(
-                            "Activity {} was not found. Use /activities to see the list.",
-                            activity
-                        ),
-                        Format::Plain,
-                    );
+                    return self
+                        .send_reply(
+                            &message,
+                            format!(
+                                "❌ Activity {} was not found. Use /activities to see the list.",
+                                activity
+                            ),
+                        )
+                        .await;
                 }
                 // Parse input in MSK timezone...
-                let start_time =
-                    parse_date_string(timespec, Local::now().with_timezone(&Moscow), Dialect::Uk);
+                let start_time = libbot::datetime::parse_time_spec(timespec);
                 // @todo Honor TELEGRAM_BOT_TIMEZONE envvar
 
                 if start_time.is_err() {
-                    return self.send_reply(
-                        &message,
-                        format!("Failed to parse time {}", timespec),
-                        Format::Plain,
-                    );
+                    return self
+                        .send_reply(&message, format!("❌ Failed to parse time {}", timespec))
+                        .await;
                 }
 
                 // ...then convert back to UTC.
-                let start_time = start_time.unwrap().with_timezone(&Utc);
+                let start_time = start_time.unwrap(); //.and_utc();
 
                 let act = act.unwrap();
 
                 log::info!("...parsed `{:?}`", start_time);
 
-                let planned_activity = NewPlannedActivity {
-                    author_id: guardian.id,
-                    activity_id: act.link,
-                    start: start_time,
+                use chrono::Offset;
+                let offset = start_time.offset().fix();
+
+                let planned_activity = plannedactivities::ActiveModel {
+                    author_id: Set(guardian.id),
+                    activity_id: Set(act.link),
+                    start: Set(start_time.with_timezone(&offset)),
+                    ..Default::default()
                 };
 
-                use diesel::result::Error;
+                // Note: Simplified without transaction for now
 
-                connection
-                    .transaction::<_, Error, _>(|| {
-                        let planned_activity = planned_activity
-                            .save(&connection)
-                            .expect("Unexpected error saving LFG group");
+                let planned_activity = planned_activity
+                    .insert(connection)
+                    .await
+                    .expect("❌ Unexpected error saving LFG group");
 
-                        let planned_activity_member = NewPlannedActivityMember {
-                            user_id: guardian.id,
-                            planned_activity_id: planned_activity.id,
-                            added: reference_date(),
-                        };
+                let planned_activity_member = plannedactivitymembers::ActiveModel {
+                    user_id: Set(guardian.id),
+                    planned_activity_id: Set(planned_activity.id),
+                    added: Set(reference_date().into()),
+                    ..Default::default()
+                };
 
-                        planned_activity_member
-                            .save(&connection)
-                            .expect("Unexpected error saving LFG group creator");
+                planned_activity_member
+                    .insert(connection)
+                    .await
+                    .expect("❌ Unexpected error saving LFG group creator");
 
-                        let activity = Activity::find_one(&connection, &act.link)
-                            .expect("Couldn't find linked activity")
-                            .unwrap();
+                let activity = activities::Entity::find_by_id(act.link)
+                    .one(connection)
+                    .await
+                    .expect("❌ Couldn't find linked activity")
+                    .unwrap();
 
-                        self.send_reply(
-                            &message,
-                            format!(
-                                "{guarName} is looking for {groupName} group {onTime}
-{joinPrompt}
-Enter `/edit{actId} details <free form description text>` to specify more details about the event.",
-                                guarName = guardian,
-                                groupName = activity.format_name(),
-                                onTime = format_start_time(start_time, reference_date()),
-                                joinPrompt = planned_activity.join_prompt(&connection),
-                                actId = planned_activity.id
-                            ),
-                            Format::Plain,
-                        );
+                let start_time = format_start_time(start_time.to_utc(), reference_date());
 
-                        Ok(())
-                    })
-                    .expect("never happens, but please implement error handling");
+                self.send_reply(
+                    &message,
+                    render_template_or_err!(
+                        "lfg/created",
+                        ("guarName" => &guardian.to_string()),
+                        ("groupName" => &activity.format_name()),
+                        ("onTime" => &start_time),
+                        ("actId" => &planned_activity.id)
+                    ),
+                )
+                .await;
             }
         }
     }
