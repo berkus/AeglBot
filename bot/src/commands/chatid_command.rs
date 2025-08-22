be26fcb391fd1a094b7fab0d1efbec000beddcf3
Berkus Decker <berkus+github@metta.systems>
2025-08-23 02:05:17 +0300

    👽 Update ChatId command

--- bot/src/commands/chatid_command.rs
+++ bot/src/commands/chatid_command.rs
@@ -1,38 +1,18 @@
-use {
-    crate::{
-        actors::bot_actor::{ActorUpdateMessage, Format, Notify, SendMessageReply},
-        commands::match_command,
-        BotCommand,
-    },
-    riker::actors::Tell,
-};
+use crate::{actors::bot_actor::ActorUpdateMessage, commands::match_command};
 
-command_actor!(ChatidCommand, [ActorUpdateMessage]);
+command_actor!(ChatIdCommand, "chatid", "Figure out the numeric chat ID");
 
-impl BotCommand for ChatidCommand {
-    fn prefix() -> &'static str {
-        "/chatid"
-    }
+impl Message<ActorUpdateMessage> for ChatIdCommand {
+    type Reply = ();
 
-    fn description() -> &'static str {
-        "Figure out the numeric chat ID"
-    }
-}
-
-impl Receive<ActorUpdateMessage> for ChatidCommand {
-    type Msg = ChatidCommandMsg;
-
-    fn receive(&mut self, _ctx: &Context<Self::Msg>, msg: ActorUpdateMessage, _sender: Sender) {
+    async fn handle(
+        &mut self,
+        msg: ActorUpdateMessage,
+        _ctx: &mut Context<Self, Self::Reply>,
+    ) -> Self::Reply {
         if let (Some(_), _) = match_command(msg.update.text(), Self::prefix(), &self.bot_name) {
-            self.bot_ref.tell(
-                SendMessageReply(
-                    format!("ChatId: {}", msg.update.chat.id),
-                    msg,
-                    Format::Plain,
-                    Notify::Off,
-                ),
-                None,
-            );
+            self.send_reply(&msg, format!("✅ Current chat id: {}", msg.update.chat.id))
+                .await;
         }
     }
 }
