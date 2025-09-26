b922be1f2d3551b4b905e1371929fec06eb73351
Berkus Decker <berkus+github@metta.systems>
2025-09-26 22:33:15 +0300

    ✨ Add language-scoped templates

--- bot/src/lib.rs
+++ bot/src/lib.rs
@@ -12,7 +12,7 @@ pub mod actors;
 pub mod commands;
 
 static TEMPLATE_FILES: std::sync::LazyLock<include_dir::Dir<'_>> =
-    std::sync::LazyLock::new(|| include_dir::include_dir!("$CARGO_MANIFEST_DIR/templates"));
+    std::sync::LazyLock::new(|| include_dir::include_dir!("$CARGO_MANIFEST_DIR/templates/en"));
 
 pub(crate) static TEMPLATES: std::sync::LazyLock<tera::Tera> = std::sync::LazyLock::new(|| {
     let mut tera = tera::Tera::default();
